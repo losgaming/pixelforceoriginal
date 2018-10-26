5013818bc77864d93ad4882560d11d1590ad4aa5
LOSDAMAN <losmuertos123@Outlook.com>
2018-10-25 21:41:05 -0500

    Assets

---
+++ PixelForce_Champions/Assets/Core/Scripts/UI/BlackScreen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackScreen : MonoBehaviour {
+
+
+    public GameObject black;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+
+        Invoke("BlackTheScreen", 10);
+		
+	}
+
+
+
+
+
+    void BlackTheScreen()
+    {
+
+        black.SetActive(true);
+
+    }
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
