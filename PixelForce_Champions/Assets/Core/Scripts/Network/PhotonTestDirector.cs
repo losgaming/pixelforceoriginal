44424591d1d581bbc0b13afab7ea72c4a5283236
LOSDAMAN <losmuertos123@Outlook.com>
2018-10-27 16:49:27 -0500

    DeveloperScene Added
    dev scene added for testing and reference on how to use photon API

---
+++ PixelForce_Champions/Assets/Core/Scripts/Network/PhotonTestDirector.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonTestDirector : MonoBehaviour {
+
+
+
+    public Transform SpawnPointTest;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+
+        PhotonNetwork.ConnectUsingSettings("0.1");
+		
+	}
+	
+
+
+    void OnConnectedToMaster ()
+    {
+
+        Debug.Log("You have connected to test master");
+        PhotonNetwork.JoinOrCreateRoom("TestRoom", null, null);
+
+    }
+
+
+
+    void OnJoinedRoom ()
+    {
+
+        Debug.Log("You have joined the test room");
+        PhotonNetwork.Instantiate("First-Person Character", SpawnPointTest.position, SpawnPointTest.rotation, 0);
+
+    }
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+}
