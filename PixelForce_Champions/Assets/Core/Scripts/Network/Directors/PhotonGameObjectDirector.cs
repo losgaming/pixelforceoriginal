1c97aa7b6204a75e76750250037836e3d16c884e
losgaming <losmuertos123@outlook.com>
2018-12-04 23:17:02 -0600

    Organized the project further
    cleaned and organized tons of assets

---
+++ PixelForce_Champions/Assets/Core/Scripts/Network/Directors/PhotonGameObjectDirector.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class PhotonGameObjectDirector : Photon.MonoBehaviour
+{
+
+
+
+
+    public GameObject PlayerCamera;
+    public MonoBehaviour[] PlayerScripts;
+    public GameObject[] gameObjects;
+    public GameObject[] gameObjects2;
+
+
+
+    private void Update()
+    {
+
+
+        if (photonView.isMine)
+        {
+
+
+
+            PlayerCamera.SetActive(true);
+
+
+
+            foreach (GameObject m in gameObjects)
+            {
+
+                m.SetActive(false);
+
+            }
+
+
+
+            foreach (GameObject m in gameObjects2)
+            {
+
+                m.SetActive(true);
+
+            }
+
+        }
+
+        else
+        {
+
+
+
+            foreach (GameObject m in gameObjects)
+            {
+
+                m.SetActive(true);
+
+            }
+
+            foreach (GameObject m in gameObjects2)
+            {
+
+                m.SetActive(false);
+
+            }
+
+
+
+            foreach (MonoBehaviour m in PlayerScripts)
+            {
+
+
+                m.enabled = false;
+
+            }
+
+
+            PlayerCamera.SetActive(false);
+
+
+
+        }
+    }
+}
